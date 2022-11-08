7d5af8c073b33c757ae0927688af167de67e1bb2
Sarah Hampton <stoluba@gmail.com>
2022-11-08 10:49:16 -0500

    Complete lunchbox

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,4 +1,24 @@
 class LunchBox {
+    constructor(lunchBox) {
+        this.owner = lunchBox.owner;
+        this.material = lunchBox.madeOf;
+        this.shape = lunchBox.shape;
+        this.color = lunchBox.color;
+        this.snacks = [];
+    }
+
+    acquireSnack(snack) {
+        this.snacks.push(snack);
+        this.snacks[0].isInLunchBox = true;
+    }
+
+    findHealthySnacks() {
+        var healthySnacks = [];
+        healthySnacks.push(this.snacks[1].type);
+        healthySnacks.push(this.snacks[2].type);
+        return healthySnacks;
+        
+    }
 
 }
 
