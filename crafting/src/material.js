271cb369bbbe5b345a5ac692633eeb990f0292ff
Sarah Hampton <stoluba@gmail.com>
2022-11-07 22:47:48 -0500

    Complete crafting

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,4 +1,24 @@
 class Material {
+    constructor(name, price, amount, units) {
+        this.name = name;
+        this.price = price;
+        this.amount = amount;
+        this.units = units;
+    }
+
+    useMaterial(number) {
+        if (number > this.amount) {
+            return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`
+        } else {
+            this.amount -= number;
+            return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+        }
+    }
+
+    calculateMaterialCost() {
+         var cost = this.price * this.amount;
+         return cost;
+    }
 
 }
 
