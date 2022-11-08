780ff5253a26f43b797383e5498a4be2f0200566
Sarah Hampton <stoluba@gmail.com>
2022-11-08 12:24:21 -0500

    Complete roadrace

--- roadrace/src/roadrace.js
+++ roadrace/src/roadrace.js
@@ -1,4 +1,28 @@
 class Roadrace {
+    constructor(roadRace) {
+        this.name = roadRace.title;
+        this.location = roadRace.city;
+        this.distance = null;
+        this.participants = [];
+
+    }
+
+    setDistance(distance) {
+        this.distance = distance;
+        return `The ${this.name} in ${this.location} is a ${distance} mile race.`;
+    }
+
+    registerParticipants(runner) {
+        this.participants.push(runner);
+    }
+
+    completeRace() {
+        for (var i = 0; i < this.participants.length; i++) {
+            this.participants[i].completedRaces.push(this.name)
+            this.participants[i].milesRun = this.distance;
+        } console.log(this.participants);
+
+    }
 
 }
 
--- roadrace/src/runner.js
+++ roadrace/src/runner.js
@@ -1,4 +1,26 @@
 class Runner {
+    constructor(name, age) {
+        this.age = age;
+        this.name = name;
+        this.milesRun = 0;
+        this.fitness = 25;
+        this.completedRaces = [];
+    }
+
+    runSomeMiles(miles) {
+        this.milesRun += miles;
+        this.fitness += miles;
+    }
+
+    stretch() {
+        this.fitness += .5;
+    }
+
+    runRace(race, length) {
+        this.completedRaces.push(race);
+        this.milesRun = length;
+        this.fitness += length
+    }
 
 }
 
